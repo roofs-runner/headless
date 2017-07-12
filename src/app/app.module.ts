ffdc84e77a1bc2e2dffb061047ec1d6de8c3d56c
Sergii Svitlookyi <sergii.svitlooky@vaimo.com>
2017-07-12 22:36:11 +0300

    Added sass support and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
